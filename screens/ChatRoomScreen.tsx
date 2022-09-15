5b482c46ffc2f0e87428460a7188e056762ca290
AhZisan <ah.zisan@gmail.com>
2022-09-15 18:50:37 +0800

    Chat screen ....

--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -1,10 +1,37 @@
 import Rect from 'react';
-import { Text } from 'react-native';
+import { Text, View, StyleSheet, FlatList } from 'react-native';
+
+import Message from '../components/Message';
+import chatRoomData from '../assets/dummy-data/Chats';
 
 
 export default function ChatRoomScreen() {
     return (
-        <Text>cxvvx</Text>) 
-    };
+        <View style={styles.page}>
+            <FlatList 
+                data={chatRoomData.messages}
+                renderItem={({ item }) => <Message message={item} />}
+                inverted
+                
+            />
+                    
+        
+        </View>               
+    ) 
+};
 
+
+const styles = StyleSheet.create({
+    page: {
+        backgroundColor: 'white',
+        flex: 1,
+
+
+    }
+
+
+
+
+
+})
 
