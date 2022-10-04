2378b02c12756e42f8ef7b7a3b170ca7fcc89e42
unknown <inzamam.cu@gmail.com>
2022-10-04 13:30:55 +0800

    login page workign

--- screens/LogInScreen.tsx
+++ screens/LogInScreen.tsx
@@ -1,91 +1,112 @@
-import { KeyboardAvoidingView, StyleSheet, Text, View, Image, Platform } from 'react-native'
-import { Button, Input } from '@rneui/base';
-import React, { useEffect, useState } from 'react'
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Platform,
+  TextInput,
+  Button,
+} from "react-native";
+// import { Button, Input } from '@rneui/base';
+import React, { useEffect, useState } from "react";
+import { signInWithEmail } from "../firebase";
 // import { auth } from '../firebase';
 // import { Input } from '@rneui/themed';
 
-const LogIn = ({navigation}) => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+const LogIn = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    // useEffect(() => {
-    //     const unsubscribe = auth.onAuthStateChanged((authUser) => {
-    //         console.log(authUser);
-    //         if (authUser) {
-    //             navigation.replace("Chats");
-    //         }
-    //     });
+  // useEffect(() => {
+  //     const unsubscribe = auth.onAuthStateChanged((authUser) => {
+  //         console.log(authUser);
+  //         if (authUser) {
+  //             navigation.replace("Chats");
+  //         }
+  //     });
 
-    //     return unsubscribe;
-    // }, []);
+  //     return unsubscribe;
+  // }, []);
 
-
-
-    const SignIn = () => {};
-    
+  const SignIn = () => {
+    signInWithEmail(email, password);
+  };
 
   return (
-    <KeyboardAvoidingView  
-        behavior={Platform.OS === "ios" ? "padding" : undefined}
-        style={styles.container}>
-            
-        <Image source={{
-            uri: "https://vectorsart.com/vectors-images/vectorsart_15220.png"}}
-            style={{width:300, height:100, borderRadius:25}}
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+      style={styles.container}
+    >
+      <Image
+        source={{
+          uri: "https://vectorsart.com/vectors-images/vectorsart_15220.png",
+        }}
+        style={{ width: 300, height: 100, borderRadius: 25 }}
+      />
+
+      <View style={styles.inputCon}>
+        {/* <Input
+              placeholder='Email' autoFocus 
+              textContentType='emailAddress'
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+           /> */}
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+          placeholder="useless placeholder"
+          keyboardType="numeric"
         />
 
-        <View style={styles.inputCon}>
-        
-        <Input
-            placeholder='Email' autoFocus 
-            textContentType='emailAddress'
-            value={email}
-            onChangeText={(text) => setEmail(text)}
-         />
-
-        <Input 
-            placeholder="Password" secureTextEntry={true}
-            textContentType='password'
-            value={password}
-            onChangeText={(text) => setPassword(text)}
-         />
-        </View>
-        <Button 
-        color= '#ff7e00'
-        containerStyle={styles.button}
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry={true}
+          textContentType="password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+      <Button
+        color="#ff7e00"
+        // style={styles.button}
         onPress={SignIn}
-        title="Login"/>
-        
-        <Button 
-        onPress={() => navigation.navigate('Register')}
-        containerStyle={styles.button} 
-        type="clear" 
-        title="Register"/>
-        
-    </KeyboardAvoidingView>
-  )
-}
+        title="Login"
+      />
 
-export default LogIn
+      <Button
+        onPress={() => navigation.navigate("Register")}
+        // style={styles.button}
+        type="clear"
+        title="Register"
+      />
+    </KeyboardAvoidingView>
+  );
+};
+
+export default LogIn;
 
 const styles = StyleSheet.create({
-    container:{
-        backgroundColor: 'white',
-        flex: 1,
-        alignItems:'center',
-        justifyContent: "center",
-        padding: 10,
-        
-        
-
-    },
-    inputCon:{
-        width: 310,
-    },
-    button: {
-        width: 200,
-        marginTop: 10,
-        
-
-    }
-})
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+  },
+  inputCon: {
+    width: 310,
+  },
+  button: {
+    width: 200,
+    marginTop: 10,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
--- screens/RegisterScreen.tsx
+++ screens/RegisterScreen.tsx
@@ -1,100 +1,106 @@
-import { KeyboardAvoidingView, Platform, StyleSheet,  View } from 'react-native'
-import React, { useState } from 'react'
-import { Button, Input, Text } from '@rneui/base';
-import { signInWithEmail } from '../firebase';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  View,
+  Text,
+} from "react-native";
+import React, { useState } from "react";
+import { signInWithEmail } from "../firebase";
 // import { auth } from '../firebase';
 // import { connectAuthEmulator } from 'firebase/auth';
 
-const RegisterScreen = ({navigation}) => {
-    const [name,setName] = useState ("");
-    const [email,setEmail] = useState ("");
-    const [password,setPassword] = useState ("");
-    const [imageUrl,setImageUrl] = useState ("");
+const RegisterScreen = ({ navigation }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
 
+  const register = () => {
+    // auth
+    // .createUserWithEmailAndPassword(email,password)
+    // .then(authUser => {
+    //     authUser.user?.updateProfile({
+    //         displayName: name,
+    //         photoURL: imageUrl ||
+    //         "https://engage.eiturbanmobility.eu/decidim-packs/media/images/default-avatar-aaa9e55bac5d7159b847.svg"
+    //     });
+    // })
+    // .catch((error) => alert(error.message));
 
+    signInWithEmail(email, password);
+  };
 
-    const register = () => {
-        // auth
-        // .createUserWithEmailAndPassword(email,password)
-        // .then(authUser => {
-        //     authUser.user?.updateProfile({
-        //         displayName: name,
-        //         photoURL: imageUrl || 
-        //         "https://engage.eiturbanmobility.eu/decidim-packs/media/images/default-avatar-aaa9e55bac5d7159b847.svg"
-        //     });
-        // })
-        // .catch((error) => alert(error.message));
-
-        signInWithEmail(email, password)
-  }
-
-
-    
   return (
-    <KeyboardAvoidingView  
-        behavior={Platform.OS === "ios" ? "padding" : undefined}
-        style={styles.container}>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+      style={styles.container}
+    >
+      <Text h3 style={{ marginBottom: 50 }}>
+        Create an Howdy account
+      </Text>
 
-            <Text h3 style={{ marginBottom: 50}}>
-                Create an Howdy account
-            </Text>
-
-            <View style={styles.inputCon}>
-                <Input 
-                placeholder='Full Name'
-                autoFocus
-                textContentType='name'
-                value={name}
-                onChangeText={(text) => setName(text)} 
-            />
-            <Input 
-                placeholder='Email'
-                textContentType='emailAddress'
-                value={email}
-                onChangeText={(text) => setEmail(text)} 
-            />
-            <Input 
-                placeholder='Password'
-                textContentType='password'
-                secureTextEntry
-                value={password}
-                onChangeText={(text) => setPassword(text)} 
-            />
-            <Input 
-                placeholder='Profile Picture URL (optional)'
-                textContentType='URL'
-                value={imageUrl}
-                onChangeText={(text) => setImageUrl(text)} 
-                onSubmitEditing={register}
-            />
-            </View>
-
-            <Button 
-                containerStyle={styles.button}
-                raised onPress={register} 
-                title="Register"
-            /> 
-
+      {/* <View style={styles.inputCon}>
+          <Input
+            placeholder="Full Name"
+            autoFocus
+            textContentType="name"
+            value={name}
+            onChangeText={(text) => setName(text)}
+          />
+          <Input
+            placeholder="Email"
+            textContentType="emailAddress"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
+          <Input
+            placeholder="Password"
+            textContentType="password"
+            secureTextEntry
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+          />
+          <Input
+            placeholder="Profile Picture URL (optional)"
+            textContentType="URL"
+            value={imageUrl}
+            onChangeText={(text) => setImageUrl(text)}
+            onSubmitEditing={register}
+          />
+        </View>
+  
+        <Button
+          containerStyle={styles.button}
+          raised
+          onPress={register}
+          title="Register"
+        /> */}
     </KeyboardAvoidingView>
-  )
-}
+  );
+};
 
-export default RegisterScreen
+export default RegisterScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 10,
-        backgroundColor: 'white'
-    },
-    inputCon: {
-        width: 310
-    },
-    button: {
-        width: 250,
-        marginTop: 10,
-    },
-
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    backgroundColor: "white",
+  },
+  inputCon: {
+    width: 310,
+  },
+  button: {
+    width: 250,
+    marginTop: 10,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
